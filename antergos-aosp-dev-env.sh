cf39a5ba34eb9b7bb7c71e0c68d4472d2e595066
Devin Shoemaker <devinshoe@gmail.com>
2016-10-16 09:09:01 -0500

    Install repo
    Install repo if it does not already exsit. Also, change the source code directory to "cm" instead of "aosp" since we will be downloading CyanogenMod for now.

--- antergos-aosp-dev-env.sh
+++ antergos-aosp-dev-env.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-# Create directories
-if [ ! -d ~/Code/android/aosp ]; then
-	mkdir -p ~/Code/android/aosp
-	echo "Created AOSP source code directory"
+# Install repo
+if [ ! -d ~/bin ]; then
+	mkdir -p ~/bin
+	echo "Created ~/bin directory"
+fi
+if [ ! -f ~/bin/repo ]; then
+	curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+	chmod a+x ~/bin/repo
+	echo "Installed repo"
+fi
+
+# Add this to ~/.bashrc
+# # set PATH so it includes user's private bin if it exists
+# if [ -d "$HOME/bin" ] ; then
+#     PATH="$HOME/bin:$PATH"
+# fi
+
+# Create source code directory
+if [ ! -d ~/Code/android/cm ]; then
+	mkdir -p ~/Code/android/cm
+	echo "Created CyanogenMod source code directory"
 fi
