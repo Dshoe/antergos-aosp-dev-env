0b72d75cbf254d42fece39b2e3684f11e7846f7d
Devin Shoemaker <devinshoe@gmail.com>
2016-10-16 19:34:31 -0500

    Install ccache

--- antergos-aosp-dev-env.sh
+++ antergos-aosp-dev-env.sh
@@ -4,23 +4,8 @@
 sudo rm /usr/bin/python
 sudo ln -s /usr/bin/python2.7 /usr/bin/python
 
-# Install repo
-yaourt -S repo
-# if [ ! -d ~/bin ]; then
-# 	mkdir -p ~/bin
-# 	echo "Created ~/bin directory"
-# fi
-# if [ ! -f ~/bin/repo ]; then
-# 	curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-# 	chmod a+x ~/bin/repo
-# 	echo "Installed repo"
-# fi
-
-# Add this to ~/.bashrc
-# # set PATH so it includes user's private bin if it exists
-# if [ -d "$HOME/bin" ] ; then
-#     PATH="$HOME/bin:$PATH"
-# fi
+# Install build tools
+yaourt -S ccache repo
 
 # Create source code directory
 if [ ! -d ~/Code/android/aosp/cm ]; then
